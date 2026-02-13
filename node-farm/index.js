e4891c19b7ee0025412591b8eabe71de8ef2e7d5
Mostafa Tabasi <dev.mostafatabasi@gmail.com>
2026-02-13 16:07:11 +0330

    exported replaceTemplate to a separate module

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,20 +1,8 @@
 const fs = require('fs')
 const http = require('http')
 const url = require('url')
+const replaceTemplate = require('./modules/replaceTemplate')
 
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
-    output = output.replace(/{%IMAGE%}/g, product.image)
-    output = output.replace(/{%PRICE%}/g, product.price)
-    output = output.replace(/{%FROM%}/g, product.from)
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
-    output = output.replace(/{%QUANTITY%}/g, product.quantity)
-    output = output.replace(/{%DESCRIPTION%}/g, product.description)
-    output = output.replace(/{%ID%}/g, product.id)
-    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
-
-    return output
-}
 
 const templateOverview = fs.readFileSync(`./node-farm/templates/overview.html`, 'utf-8')
 const templatePrduct = fs.readFileSync(`./node-farm/templates/product.html`, 'utf-8')
@@ -42,7 +30,7 @@ const server = http.createServer((req, res) => {
 
         const product = dataObject[query.id]
         const output = replaceTemplate(templatePrduct, product)
-        
+
         res.end(output)
 
         // API
