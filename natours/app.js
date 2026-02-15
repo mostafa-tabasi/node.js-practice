42228f4f8bb8eb52c8c0f23d04596d84318cd052
Mostafa Tabasi <dev.mostafatabasi@gmail.com>
2026-02-15 19:31:51 +0330

    added tours post api (to add a new tour)

--- natours/app.js
+++ natours/app.js
@@ -2,16 +2,38 @@ const fs = require("fs");
 const express = require("express");
 const app = express();
 
+// the middleware we need to read the request body
+app.use(express.json());
+
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
+app.get("/api/v1/tours", (req, res) => {
     res.status(200).json({
         status: "success",
         results: tours.length,
         data: { tours }
     });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+
+    tours.push(newTour);
+
+    fs.writeFile(
+        `${__dirname}/dev-data/tours-simple.json`,
+        JSON.stringify(tours),
+        err => {
+            res.status(201).json({
+                status: 'success',
+                data: {
+                    tour: newTour
+                }
+            })
+        })
 });
 
 const port = 3000;
