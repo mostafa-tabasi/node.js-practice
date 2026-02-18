4355a325ba18474f33d1e64514293fbcf08df069
Mostafa Tabasi <dev.mostafatabasi@gmail.com>
2026-02-18 23:34:31 +0330

    connected to local database using mongoose

--- natours/server.js
+++ natours/server.js
@@ -1,6 +1,24 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  // .connect(DB, {
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+  })
+  .then((con) => {
+    console.log("Database connection successful!");
+  })
+  .catch((err) => {
+    console.error("DB connection error:", err);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
