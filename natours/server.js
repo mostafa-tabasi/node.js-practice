a1488ddc5313511e0b4aa56d4767376687a5ed68
Mostafa Tabasi <dev.mostafatabasi@gmail.com>
2026-02-18 23:54:24 +0330

    changes:
    - defined tour schema.
    - made a test tour out of tour model

--- natours/server.js
+++ natours/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
+// The remote database currently not working
 const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
   process.env.DATABASE_PASSWORD,
@@ -18,6 +19,39 @@ mongoose
   })
   .catch((err) => {
     console.error("DB connection error:", err);
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A tour must have name"],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, "A tour must have a price"],
+  },
+});
+
+const Tour = mongoose.model("Tour", tourSchema);
+
+const testTour = new Tour({
+  name: "The Forest Hiker",
+  rating: 4.7,
+  price: 497,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(`ERROR: ${err}`);
   });
 
 const port = process.env.PORT || 3000;
