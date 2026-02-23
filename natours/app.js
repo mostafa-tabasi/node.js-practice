e696f100b4de5e80f05870f3115c918a57fa4d5b
Mostafa Tabasi <dev.mostafatabasi@gmail.com>
2026-02-23 12:53:12 +0330

    added a general error handler middleware

--- natours/app.js
+++ natours/app.js
@@ -25,9 +25,21 @@ app.use("/api/v1/users", userRouter);
 // because the middlewares will be applied by their order.
 // so it means the route isn't handled by devs and need to show a generic response of 404.
 app.all("*", (req, res, next) => {
-  res.status(404).json({
-    status: "fail",
-    message: `Can't find ${req.originalUrl} on this server!`,
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = "fail";
+  err.statusCode = 404;
+
+  next(err);
+});
+
+// a general error handler middleware
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
