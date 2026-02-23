87877ad0440863918c9ea6ac6a809dc9cc3fc19a
Mostafa Tabasi <dev.mostafatabasi@gmail.com>
2026-02-23 21:04:00 +0330

    handled 404 error for get tour, update tour and, delete tour controllers

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -2,6 +2,7 @@
 const Tour = require("./../models/tourModel");
 const APIFeatures = require("./../utils/apiFeatures");
 const catchAsync = require("./../utils/catchAsync");
+const AppError = require("./../utils/appError");
 
 /*
 const tours = JSON.parse(
@@ -65,6 +66,10 @@ exports.getTour = catchAsync(async (req, res, next) => {
   const id = req.params.id;
   const tour = await Tour.findById(id);
 
+  if (!tour) {
+    return next(new AppError("No tour found with this ID", 404));
+  }
+
   res.status(200).json({
     status: "success",
     data: {
@@ -90,6 +95,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     runValidators: true,
   });
 
+  if (!tour) {
+    return next(new AppError("No tour found with this ID", 404));
+  }
+
   res.status(200).json({
     status: "success",
     data: {
@@ -99,7 +108,11 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  await Tour.findByIdAndDelete(req.params.id);
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+
+  if (!tour) {
+    return next(new AppError("No tour found with this ID", 404));
+  }
 
   res.status(204).json({
     status: "success",
