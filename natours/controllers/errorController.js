cc8d47929f7c8ba5ba4db91c2c12efd16618baaa
Mostafa Tabasi <dev.mostafatabasi@gmail.com>
2026-02-24 20:23:01 +0330

    handled duplicate value error on adding a new tour endpoint

--- natours/controllers/errorController.js
+++ natours/controllers/errorController.js
@@ -5,6 +5,14 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = (err) => {
+  // .match() has some issue, so we commented this function usage for now
+  const value = err.message.match(/(["'])(\\?.)*?\1/)[0];
+  console.log(value);
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     error: err,
@@ -41,7 +49,10 @@ module.exports = (err, req, res, next) => {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === "production") {
     let error = { ...err };
+
     if (error.name === "CastError") error = handleCastErrorDB(error);
+    // if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+
     sendErrorProd(error, res);
   }
 };
