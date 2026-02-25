31da033e811a0f35ac08ae024d16a85301fa52f4
Mostafa Tabasi <dev.mostafatabasi@gmail.com>
2026-02-25 23:08:19 +0330

    added token to signup endpoint

--- natours/controllers/authController.js
+++ natours/controllers/authController.js
@@ -1,11 +1,24 @@
 const User = require("./../models/userModel");
+const jwt = require("jsonwebtoken");
 const catchAsync = require("./../utils/catchAsync");
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  // we use this way of user creation instead of "User.create(req.body)" so that
+  // we only get and store the fields we need instead of whatever user sends in request body
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(201).json({
     status: "success",
+    token,
     data: {
       user: newUser,
     },
